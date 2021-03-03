f3917acdbc3d7538404d58362bb09ea72fba0f23
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2021-03-03 21:06:09 +0100

    changed name of the app

--- TestCheckbox/TestCheckbox.Android/MainActivity.cs
+++ TestCheckbox/TestCheckbox.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using TestCheckbox.Droid;
 
 namespace AppBaseNamespace.Droid
 {
-    [Activity(Label = "TestCheckbox", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "TrainingApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
