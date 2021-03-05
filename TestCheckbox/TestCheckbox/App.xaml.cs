ddbd2e325e8801f8f886deeaec38034d232f7c61
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2021-03-05 14:44:06 +0100

    added skeletons of methods for resource synchronization

--- TestCheckbox/TestCheckbox/App.xaml.cs
+++ TestCheckbox/TestCheckbox/App.xaml.cs
@@ -20,17 +20,22 @@ namespace AppBaseNamespace
         Dictionary<string, string> shortcuts = new Dictionary<string, string>();
         public App()
         {
+            InitializeShortcuts();
+            InitializeComponent();
+            RetrieveUserSettings(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            SetAppLanguage(userSettings.AppLanguage);
+            SynchronizeResources();
+            MainPage = new NavigationPage(new MainPage(this, userSettings.AppLanguage));
+        }
+
+        private void InitializeShortcuts()
+        {
             shortcuts.Add("English", "en");
             shortcuts.Add("German", "de");
             shortcuts.Add("Czech", "cs");
             shortcuts.Add("French", "fr");
             shortcuts.Add("Chinese", "zh-Hans");
-            InitializeComponent();
-            userSettings = RetrieveUserSettings(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
-            SetAppLanguage(userSettings.AppLanguage);
-            MainPage = new NavigationPage(new MainPage(this, userSettings.AppLanguage));
         }
-
         private void SetAppLanguage(string languageName) 
         {              
             string shortcut;
@@ -50,15 +55,15 @@ namespace AppBaseNamespace
             
         }
 
-        private UserSettings RetrieveUserSettings(string path)
+        private void RetrieveUserSettings(string path)
         {
             UserSettings result = new UserSettings(path);
             if (File.Exists(fileName))
             {
                 result = Newtonsoft.Json.JsonConvert.DeserializeObject<UserSettings>(File.ReadAllText(fileName).Trim());
-            }          
-            
-            return result;
+            }
+
+            userSettings = result;
         }
 
         public void ReloadApp(string language, string previouslyChecked)
@@ -68,6 +73,43 @@ namespace AppBaseNamespace
             Application.Current.Properties["currentLanguage"] = language;
             File.WriteAllText(fileName, Newtonsoft.Json.JsonConvert.SerializeObject(userSettings));
             MainPage = new NavigationPage(new MainPage(this, previouslyChecked));
+        }
+
+        void SynchronizeResources()
+        {
+            switch (userSettings.UpdateInterval)
+            {
+                case "Automatic":
+                    HandleAutomaticUpdate(DateTime.Now);
+                    break;
+                case "Once a month":
+                    HandleOnceAMonthUpdate(DateTime.Now);
+                    break;
+                case "On request":                    
+                    break;
+                default:
+                    HandleAutomaticUpdate(DateTime.Now);
+                    break;
+            }
+        }
+
+        public  void HandleOnRequestUpdate(DateTime now)
+        {
+            //toto tu sa bude volat po kliknuti na button request update
+        }
+
+        private void HandleOnceAMonthUpdate(DateTime now)
+        {           
+            if (now.Subtract(userSettings.DateOfLastUpdate).TotalDays > 28)
+            {                
+                HandleAutomaticUpdate(now);
+            }
+        }
+
+        private void HandleAutomaticUpdate(DateTime now)
+        {
+            userSettings.DateOfLastUpdate = now;
+            //tuto sa bude so serverom komunikovat
         }
 
         public void SaveUserSettings()
