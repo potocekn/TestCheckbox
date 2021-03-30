5855acf921b68c1cf70e8b8df283fba53c60dab1
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2021-03-30 23:45:02 +0200

    new class representing necessary resources information

---
+++ TestCheckBox/TestCheckBox/UserSettingsHelpers/ResourcesInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBase.UserSettingsHelpers
+{
+    public class ResourcesInfo
+    {
+        public string Language { get; set; }
+        public string ResourceName { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string Url { get; set; }
+    }
+}
