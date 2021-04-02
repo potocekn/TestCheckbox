e25f0c0762a1e2d0a1c6d91971101b47ec795ee2
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2021-04-02 22:49:23 +0200

    added html showing in webview (no pictures yet)

---
+++ TestCheckBox/TestCheckBox/ViewModels/ResourcesPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using AppBaseNamespace;
+using Plugin.XamarinFormsSaveOpenPDFPackage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppBase.ViewModels
+{
+    public class ResourcesPageViewModel
+    {
+        App app { get; set; }
+        public Command GoToPDFs { get; set; }
+        public Command GoToHTMLs { get; set; }
+        public Command GoToHTMLsLabel { get; set; }
+
+        public ResourcesPageViewModel(App app, INavigation navigation)
+        {
+            this.app = app;
+            GoToPDFs = new Command(() => {
+                navigation.PushAsync(new PDFPage(app));
+            });
+            GoToHTMLs = new Command(() => {
+                navigation.PushAsync(new HTMLPage(app));
+            });
+        }
+
+        async void LoadAndOpenResource()
+        {
+            var httpClient = new HttpClient();
+            var stream = await httpClient.GetStreamAsync("http://www.4training.net/mediawiki/images/a/af/Gods_Story_%28five_fingers%29.pdf");
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await stream.CopyToAsync(memoryStream);
+                await CrossXamarinFormsSaveOpenPDFPackage.Current.SaveAndView("test.pdf", "application/pdf", memoryStream, PDFOpenContext.InApp);
+            }
+
+                
+        }
+    }
+}
