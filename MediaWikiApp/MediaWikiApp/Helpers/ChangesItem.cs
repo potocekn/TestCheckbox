8a2cf5c8aa8d541c3f766b9cbee22da3ec18d7bd
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2021-05-18 21:03:05 +0200

    new class for working with the changes.json files in github folders

---
+++ MediaWikiApp/MediaWikiApp/Helpers/ChangesItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBase.Helpers
+{
+    class ChangesItem
+    {
+        public string FileName { get; set; }
+        public int VersionNumber { get; set; }
+    }
+}
