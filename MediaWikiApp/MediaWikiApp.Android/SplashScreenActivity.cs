6e616e0a0b5b3079678d2ef9dadb58090ed518fb
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2021-06-23 20:23:02 +0200

    added splash screen for android

---
+++ MediaWikiApp/MediaWikiApp.Android/SplashScreenActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroidX.AppCompat.App;
+using AppBaseNamespace.Droid;
+
+namespace TestCheckbox.Droid
+{
+    [Activity(Theme = "@style/SplashScreen", MainLauncher = true, NoHistory = true)]
+    class SplashScreenActivity: AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState) 
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            try
+            {
+                base.OnResume();
+                var intent = new Intent(this, typeof(MainActivity));
+                if (Intent.Extras != null)
+                {
+                    intent.PutExtras(Intent.Extras);
+                }
+
+                StartActivity(intent);
+            }
+            catch
+            { 
+            }
+        }
+    }
+}
